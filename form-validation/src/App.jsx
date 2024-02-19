89ab2501ff3428d31c040b29e458e12338c7d7c8
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2024-02-19 18:55:17 +0530

    FVYUP

---
+++ form-validation/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import FormWOYUP from './components/FormWOYUP'
+
+function App() {
+  return (
+    <div>
+        <FormWOYUP/>
+    </div>
+  )
+}
+
+export default App
