735e5d2959bca0e951d7c9fbc7fb217068e6b6b0
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2024-02-29 00:59:53 +0530

    Carousels

--- Multiple-Ques/src/Components/Carousels/Carousel.jsx
+++ Multiple-Ques/src/Components/Carousels/Carousel.jsx
@@ -1,11 +1,76 @@
 import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import img1 from '../images/image1.jpg';
+import img2 from '../images/image2.jpg';
+import img3 from '../images/image3.jpg';
+import img4 from '../images/image4.jpg';
+import img5 from '../images/image5.jpg';
+import img6 from '../images/image6.jpg';
 
 function Carousel() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      };
   return (
-    <div>
-      Carouselss
+    <div className='w-3/4 m-auto'>
+      <div className="mt-20">
+      <Slider {...settings}>
+        {data.map((d) => (
+          <div key={d.name} className="bg-white h-[450px] text-black rounded-xl">
+            <div className='h-56 bg-indigo-500 flex justify-center items-center rounded-t-xl'>
+              <img src={d.img} alt="" className="h-44 w-44 rounded-full"/>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-4 p-4">
+              <p className="text-xl font-semibold">{d.name}</p>
+              <p className="text-center">{d.review}</p>
+              <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      </div>
     </div>
   )
 }
+
+const data = [
+    {
+      name: `John Morgan`,
+      img: img1,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+      name: `Ellie Anderson`,
+      img: img2,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+      name: `Nia Adebayo`,
+      img: img3,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+      name: `Rigo Louie`,
+      img: img4,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+      name: `Mia Williams`,
+      img: img5,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+      name: `Maria Williams`,
+      img: img6,
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    
+  ];
 
 export default Carousel
